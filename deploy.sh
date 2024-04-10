f758c4b0a2726fb59eaab8a180543ae6782dd3f7
DevopsPractice <nmokshithvarma@gmail.com>
2024-04-10 12:54:55 +0530

    test 11

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /home/ec2-user/tomcat/webapps/*
+
 # Copy application artifact to Tomcat webapps directory
 cp /home/ec2-user/server/target/firstproject123-1.0-SNAPSHOT.war /home/ec2-user/tomcat/webapps/firstproject
 
