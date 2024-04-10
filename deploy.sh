8379222599d62a89fd103984bbe3e94a84b47f05
DevopsPractice <nmokshithvarma@gmail.com>
2024-04-10 12:19:55 +0530

    test5

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copy application artifact to Tomcat webapps directory
+cp /home/ec2-user/server/target/application.war /home/ec2-user/tomcat/webapps/firstproject
+
+# Change directory to Tomcat bin folder
+cd /home/ec2-user/tomcat/bin
+
+# Stop Tomcat
+./shutdown.sh
+
+# Wait for 20 seconds
+sleep 20
+
+# Start Tomcat
+./startup.sh
