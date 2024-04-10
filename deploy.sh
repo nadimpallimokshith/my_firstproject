bddff8542ad9477e02f37027fb2671812b34207f
DevopsPractice <nmokshithvarma@gmail.com>
2024-04-10 12:36:55 +0530

    test-7

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy application artifact to Tomcat webapps directory
-cp /home/ec2-user/server/target/application.war /home/ec2-user/tomcat/webapps/firstproject
+cp /home/ec2-user/server/target/firstproject123-1.0-SNAPSHOT.war /home/ec2-user/tomcat/webapps/firstproject
 
 # Change directory to Tomcat bin folder
 cd /home/ec2-user/tomcat/bin
