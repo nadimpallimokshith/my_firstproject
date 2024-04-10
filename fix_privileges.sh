c222271d5f96d3ca3b2194fc4b7451c6b8a61943
DevopsPractice <nmokshithvarma@gmail.com>
2024-04-10 12:06:10 +0530

    test 3

--- fix_privileges.sh
+++ fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/target/* war
+chmod +x /home/ec2-user/server/* war
 chmod +x /home/ec2-user/server/server_start.sh
 chmod +x /home/ec2-user/server/server_stop.sh
