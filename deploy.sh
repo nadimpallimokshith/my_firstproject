118911bd5d24aa3eac840a10af235f75e2395881
nadim <nadim@DESKTOP-F2J6HCM.mshome.net>
2024-05-11 12:36:38 +0530

    test 2

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-sudo su
-rm -rf /home/ec2-user/tomcat/webapps/*
+sudo rm -rf /home/ec2-user/tomcat/webapps/*
 
 # Copy application artifact to Tomcat webapps directory
-sudo su
-cp /home/ec2-user/server/target/firstproject123-1.0-SNAPSHOT.war /home/ec2-user/tomcat/webapps/
+sudo cp /home/ec2-user/server/target/firstproject123-1.0-SNAPSHOT.war /home/ec2-user/tomcat/webapps/
 
 # Change directory to Tomcat bin folder
-sudo su
 cd /home/ec2-user/tomcat/bin
 
 # Stop Tomcat
-./shutdown.sh
+sudo ./shutdown.sh
 
 # Wait for 20 seconds
 sleep 20
 
 # Start Tomcat
-./startup.sh
+sudo ./startup.sh
