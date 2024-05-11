9562a3aee8fae72379faa2ec31f164cc970e43ba
nadim <nadim@DESKTOP-F2J6HCM.mshome.net>
2024-05-11 12:31:59 +0530

    tst

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+sudo su
 rm -rf /home/ec2-user/tomcat/webapps/*
 
 # Copy application artifact to Tomcat webapps directory
+sudo su
 cp /home/ec2-user/server/target/firstproject123-1.0-SNAPSHOT.war /home/ec2-user/tomcat/webapps/
 
 # Change directory to Tomcat bin folder
+sudo su
 cd /home/ec2-user/tomcat/bin
 
 # Stop Tomcat
