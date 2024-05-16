8d8dccc5c141214c07e1b83105c4db760dceb424
nadim <nadim@192.168.1.251>
2024-05-16 11:53:22 +0530

    twst

--- AfterInstall.ps1
+++ AfterInstall.ps1
@@ -2,7 +2,7 @@
 # This script contains tasks to be performed after the application has been installed
 
 # Start any services or processes required by your application
-Start-Service -Name "YourServiceName"
-Start-Process -FilePath "C:\Path\To\Your\Application\YourExecutable.exe" -NoNewWindow
+# Start-Service -Name "YourServiceName"
+# Start-Process -FilePath "C:\Path\To\Your\Application\YourExecutable.exe" -NoNewWindow
 
 # Optionally, you can perform other configuration tasks here
