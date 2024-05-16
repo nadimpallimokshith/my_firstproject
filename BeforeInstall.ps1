8d70cad07f02bba708520ca15cf5174af239e848
nadim <nadim@192.168.1.251>
2024-05-16 12:30:09 +0530

    test

--- BeforeInstall.ps1
+++
@@ -1,8 +0,0 @@
-# Example BeforeInstall.ps1 script
-# This script can contain any tasks you need to perform before installing your application
-
-# Stop any services or processes that might interfere with the installation process
-# Stop-Service -Name "YourServiceName" -Force
-# Stop-Process -Name "YourProcessName" -Force
-
-# Optionally, you can perform other setup tasks here
