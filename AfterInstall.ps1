771007a320734ef543ae99bbf3432b719226023e
nadim <nadim@192.168.1.251>
2024-05-16 11:37:55 +0530

    test

---
+++ AfterInstall.ps1
@@ -0,0 +1,8 @@
+# Example AfterInstall.ps1 script
+# This script contains tasks to be performed after the application has been installed
+
+# Start any services or processes required by your application
+Start-Service -Name "YourServiceName"
+Start-Process -FilePath "C:\Path\To\Your\Application\YourExecutable.exe" -NoNewWindow
+
+# Optionally, you can perform other configuration tasks here
---
+++ ApplicationStart.ps1
@@ -0,0 +1,7 @@
+# Example ApplicationStart.ps1 script
+# This script contains tasks to be performed after the application has started
+
+# Optionally, you can perform additional tasks or checks to ensure the application is running correctly
+Write-Host "Application started successfully."
+
+# Optionally, you can perform additional monitoring or logging tasks here
